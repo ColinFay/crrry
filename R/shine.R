a87dda7ac59772bf595600e21ccdbbab305ea443
colin <colin@thinkr.fr>
2020-04-13 13:41:04 +0200

    sleep_while

--- R/shine.R
+++ R/shine.R
@@ -13,6 +13,21 @@ sleep_while_shiny_busy <- function(Runtime, timeout = 1){
   }
 }
 
+sleep_while <- function(cond, Runtime, timeout = 1){
+  cli::cat_line("Waiting for cond")
+  repeat {
+    #browser()
+    res <- Runtime$evaluate(
+      expression = cond
+    )
+    res <- crrri::hold(res)
+    if (res$result$value){
+      break()
+    }
+    Sys.sleep(timeout)
+  }
+}
+
 check_still_running <- function(Runtime){
   res <- Runtime$evaluate(
     expression = 'document.getElementById("shiny-disconnected-overlay") !== null'
